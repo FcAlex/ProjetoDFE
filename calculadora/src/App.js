c80ef30e824fbe3bca2b481fb8714feae7c00f08
FcAlex <contato.fcoalex@gmail.com>
2021-05-31 18:07:59 -0300

    adiciona estilo

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+import './App.css'
 
 class App extends Component {
 
@@ -16,10 +17,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1>{process.env.REACT_APP_TITLE}</h1>
-        <input type="number" value={this.state.n1} onChange={e => this.setState({n1: +e.target.value})} />
-        <input type="number" value={this.state.n2} onChange={e => this.setState({n2: +e.target.value})} />
+        <div className="inputs">
+          <input type="number" value={this.state.n1} onChange={e => this.setState({n1: +e.target.value})} />
+          <input type="number" value={this.state.n2} onChange={e => this.setState({n2: +e.target.value})} />
+        </div>
         <button onClick={this.sum}>+</button>
-        <h2>{this.state.result}</h2>
+        <span>Resultado: <strong>{this.state.result}</strong> </span>
       </div>
     );
   }
